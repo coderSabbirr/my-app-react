ee095d505bcb20c74f23b749b2394bb5c4aa7808
Nahid Hasan Sabbir <sabbirnahid5817@gmail.com>
2021-09-19 01:27:57 +0600

    first coimmit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Mycomponent band="Apple" price="10000"></Mycomponent>
+     <Mycomponent band="Microsoft" price="1550"></Mycomponent>
+     <Mycomponent band="google" price="00"></Mycomponent>
+     <Mycomponent band="SpaceX" price="5000"></Mycomponent>
+     <Mycomponent></Mycomponent>
+
+
     </div>
   );
+}
+
+
+function Mycomponent(props) {
+  const [points,setPoints] = useState(1)
+
+  const handalAddPoints = () => {
+    const newPoints = points * 2
+    setPoints(newPoints)
+  } 
+  console.log(props);
+
+ return (
+<div className="mystyle">
+  <h1>Band: {props.band}</h1>
+  <p>Price {props.price} , Points: {points}</p>
+  <button onClick={handalAddPoints}>Add Points</button>
+  <p>It sets up your development environment so that you can use the latest JavaScript features, provides a nice developer experience, and optimizes your</p>
+</div>
+
+ );
 }
 
 export default App;
